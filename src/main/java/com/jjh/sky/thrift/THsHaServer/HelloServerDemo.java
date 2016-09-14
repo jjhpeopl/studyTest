fd2ab767758f13533cfafbae1ba204f2fb05244c
jiajianhong <jiajianhong@meituan.com>
2016-09-14 14:48:27 +0800

    thrift

---
+++ src/main/java/com/jjh/sky/thrift/THsHaServer/HelloServerDemo.java
@@ -0,0 +1,50 @@
+package com.jjh.sky.thrift.THsHaServer;
+
+import com.jjh.sky.thrift.demo.HelloWorldService;
+import com.jjh.sky.thrift.demo.impl.HelloWorldImpl;
+import org.apache.thrift.TProcessor;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.server.THsHaServer;
+import org.apache.thrift.transport.TFramedTransport;
+import org.apache.thrift.transport.TNonblockingServerSocket;
+
+/**
+ * Created by jiajianhong on 16/9/6.
+ * 半同步半异步的服务端模型
+ */
+public class HelloServerDemo {
+
+    public static final int SERVER_PORT = 8090;
+
+    public void startServer() {
+        try {
+
+            System.out.println("HelloWorld THsHaServer start...");
+
+            TProcessor processor = new HelloWorldService.Processor<HelloWorldService.Iface>(new HelloWorldImpl());
+
+            // 打开端口
+            TNonblockingServerSocket socket = new TNonblockingServerSocket(SERVER_PORT);
+
+            // 封装参数
+            THsHaServer.Args args = new THsHaServer.Args(socket);
+            args.processor(processor);
+            args.transportFactory(new TFramedTransport.Factory());
+            args.protocolFactory(new TBinaryProtocol.Factory());
+
+            // 半同步半异步的服务模型
+            THsHaServer server = new THsHaServer(args);
+            server.serve();
+        } catch (Exception e) {
+            System.out.println("THsHaServer start error!!!");
+            e.printStackTrace();
+
+        }
+    }
+
+    public static void main(String[] args) {
+        HelloServerDemo helloServerDemo = new HelloServerDemo();
+        helloServerDemo.startServer();
+    }
+
+}
