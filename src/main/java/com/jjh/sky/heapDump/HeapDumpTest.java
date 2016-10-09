1c466258c4e55883942c52120447112802fcfa97
jiajianhong <jiajianhong@meituan.com>
2016-10-09 11:36:09 +0800

    heapdump

--- src/main/java/com/jjh/sky/heapDump/HeapDumpTest.java
+++ src/main/java/com/jjh/sky/heapDump/HeapDumpTest.java
@@ -6,9 +6,15 @@ package com.jjh.sky.heapDump;
 public class HeapDumpTest {
 
     public static void main(String[] args) {
-        long arr[];
+        long arr[] = new long[0];
         for (int i=1; i<=10000000; i*=2) {
             arr = new long[i];
+
+            System.out.println("size : " + i);
+            Runtime runtime = Runtime.getRuntime();
+            System.out.printf("maxMemory : %.2fM\n", runtime.maxMemory()*1.0/1024/1024);
+            System.out.printf("totalMemory : %.2fM\n", runtime.totalMemory()*1.0/1024/1024);
+            System.out.printf("freeMemory : %.2fM\n", runtime.freeMemory()*1.0/1024/1024);
         }
     }
 
