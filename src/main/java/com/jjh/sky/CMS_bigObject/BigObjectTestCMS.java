32755ef01230095a533adcd93fac7b5b6605a0e7
jiajianhong <jiajianhong@meituan.com>
2016-10-12 11:42:05 +0800

    bigObject

--- src/main/java/com/jjh/sky/CMS_bigObject/BigObjectTestCMS.java
+++ src/main/java/com/jjh/sky/CMS_bigObject/BigObjectTestCMS.java
@@ -4,10 +4,24 @@ import java.util.ArrayList;
 
 /**
  * Created by jjh on 16/10/11.
+ * -verbose:gc
+ -Xms100M
+ -Xmx100M
+ -Xmn30M
+ -XX:+PrintGCDetails
+ -XX:SurvivorRatio=8
+ -XX:+UseConcMarkSweepGC
+ -XX:+UseParNewGC
+ -XX:+PrintCommandLineFlags
  */
 public class BigObjectTestCMS {
 
+    private static final int _1MB = 1024 * 1024;
+
     public static void main(String[] args) {
+
+
+
         try {
             Thread.sleep(15000);
         } catch (InterruptedException e) {
@@ -15,7 +29,7 @@ public class BigObjectTestCMS {
         }
 
         System.out.println("start");
-        long[] longs = new long[100000];
+        byte[] longs = new byte[24 * _1MB];
 
         try {
             Thread.sleep(120000);
